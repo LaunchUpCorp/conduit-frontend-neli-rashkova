2a73aebf403380dc10eec7e219a30715f2236b99
neli-rashkova <neli.rashkova73@gmail.com>
2022-10-24 17:19:48 -0400

    modified font family for h1 element to font anton

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Header() {
     return (
-        <div className="bg-emerald-500 w-full">
+        <div className="bg-green-500 bg-opacity-95 w-full mb-2.5 pt-2">
             <div className="mx-auto flex flex-col justify-center items-center">
-                <h1 className="font-sans font-extrabold text-4xl text-white text-center mt-4">conduit</h1>
-                <p className="font-sans text-small font-light text-white text-center mb-3 mt-1">A place to share your knowledge.</p>
+                <h1 className="font-anton text-4xl text-white text-center tracking-wider mt-4">conduit</h1>
+                <p className="font-sans text-small font-light text-white text-center mb-3.5 mt-1">A place to share your knowledge.</p>
             </div>
         </div>
     );
